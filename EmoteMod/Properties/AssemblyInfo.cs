788f274d337909eb6058affed1f56ec60d28d4c4
xDesantx <wazzap203203@list.ru>
2020-09-28 21:36:29 +0300

    minor changes in files

--- EmoteMod/Properties/AssemblyInfo.cs
+++ EmoteMod/Properties/AssemblyInfo.cs
@@ -8,7 +8,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyTitle("EmoteMod")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyCompany("DesantUa")]
 [assembly: AssemblyProduct("EmoteMod")]
 [assembly: AssemblyCopyright("Copyright ©  2020")]
 [assembly: AssemblyTrademark("")]
