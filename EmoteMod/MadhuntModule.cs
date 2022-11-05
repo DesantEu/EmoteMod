b87e1424c133d5e0f4b6e57315fc8cfff058d83b
DesantEu <wazzap203203@list.ru>
2022-11-05 02:42:45 +0100

    imma save this lmao

--- EmoteMod/MadhuntModule.cs
+++ EmoteMod/MadhuntModule.cs
@@ -14,6 +14,7 @@ namespace Celeste.Mod.EmoteMod
 
 		private static ILHook onMadhuntStartRoundHook;
 		private static ILHook onMadhuntStopRoundHook;
+		private static ILHook onMadhuntGetInRoundHook;
 
 		public static bool inRound = false;
 
@@ -31,20 +32,116 @@ namespace Celeste.Mod.EmoteMod
 				MethodInfo roundStartHook = madhuntModule.GetType().Assembly.GetType("Celeste.Mod.Madhunt.Manager").GetMethod("StartRound");
 				onMadhuntStartRoundHook = new ILHook(roundStartHook, onMadhuntStartRound);
 
-				MethodInfo roundEndHook = madhuntModule.GetType().Assembly.GetType("Celeste.Mod.Madhunt.Manager").GetMethod("StopRound");
-				onMadhuntStopRoundHook = new ILHook(roundEndHook, onMadhuntStopRound);
+				//MethodInfo roundEndHook = madhuntModule.GetType().Assembly.GetType("Celeste.Mod.Madhunt.Manager").GetMethod("StopRound");
+				//onMadhuntStopRoundHook = new ILHook(roundEndHook, onMadhuntStopRound);
+
+				foreach (PropertyInfo m in madhuntModule.GetType()
+					.GetProperty("Instance")
+					.GetValue(null).GetType()
+					.GetProperty("MadhuntManager").GetValue(null).GetType()
+					.GetProperties())
+				{
+					test += m.Name + "; ";
+				}
+
+				
+				On.Celeste.Level.LoadLevel += Level_LoadLevel;
+
+				MethodInfo getInRoundHook = madhuntModule.GetType().Assembly.GetType("Celeste.Mod.Madhunt.Manager").GetProperty("InRound").GetMethod;
+				onMadhuntGetInRoundHook = new ILHook(getInRoundHook, onMadhuntGetInRound);
+
+
 			}
+		}
+
+		private static void onMadhuntGetInRound(ILContext il)
+		{
+			ILCursor cursor = new ILCursor(il);
+
+			cursor.TryGotoNext(MoveType.After, instr => instr.MatchRet());
+			cursor.Index--;
+
+			cursor.EmitDelegate<Func<bool,bool>>( (r) => { inRound = r; return r; }); 
+		}
+
+		private static void Level_LoadLevel(On.Celeste.Level.orig_LoadLevel orig, Level self, Player.IntroTypes playerIntro, bool isFromLoader)
+		{
+			orig(self, playerIntro, isFromLoader);
+
+			EmoteModMain.echo("level loaded");
+			if (madhuntLoaded)
+			{
+				//bool asd = (bool)Type.GetType("Celeste.Mod.Madhunt.Manager").GetProperty("InRound").GetValue(Type.GetType("Celeste.Mod.Madhunt.Module").GetProperty("MadhuntManager").GetValue(null));
+				inRound = (bool)madhuntModule.GetType().Assembly.GetType("Celeste.Mod.Madhunt.Manager").GetProperty("InRound").GetValue(madhuntModule.GetType().GetProperty("MadhuntManager").GetValue(null));
+
+
+				EmoteModMain.echo(asd.ToString());
+
+				//EmoteModMain.echo(Type.GetType("Celeste.Mod.Madhunt.Manager").ToString());
+
+				//madhuntModule.GetType().getGetType().GetField("Instance").GetType()
+				//	.GetMethod("get_MadhuntManager").Invoke(null,null)
+				//	.GetType().GetMethod("get_InGame").Invoke(null,null);
+
+
+				//PropertyInfo inRoundProp = madhuntModule.GetType()
+				//	//.GetProperty("Instance")
+				//	//.GetValue(null).GetType()
+				//	.GetProperty("MadhuntManager").GetValue(null).GetType()
+
+				//	.GetProperty("InRound");
+
+
+				//if (madhuntModule.GetType().GetProperty("MadhuntManager").GetValue(null) == null) EmoteModMain.echo("LMAO ASDFIGHKUYSADHIKGJASKFHJG");
+
+
+				//else
+				//try
+				//{
+				//	EmoteModMain.echo(inRoundProp.GetGetMethod().Invoke(null,null).ToString());
+				//}catch (Exception ex)
+				//{
+				//	EmoteModMain.echo(ex.ToString());
+				//}
+
+				//EmoteModMain.echo(madhuntModule.GetType().GetProperty("MadhuntManager").GetValue(null).GetType().Name);
+
+
+
+			}
+			//EmoteModMain.echo(Madhunt.Module.MadhuntManager.InRound.ToString());
+
 		}
 
 		public static void onMadhuntStartRound(ILContext il)
 		{
 			ILCursor cursor = new ILCursor(il);
 
-			// go before return true
+			//// go before return
 			cursor.TryGotoNext(MoveType.After, instr => instr.MatchRet());
-			cursor.Index-=3;
+			//// steal the return value
+			//cursor.Index--;
+			//cursor.Emit(cursor.Prev.OpCode, cursor.Prev.Operand);
+			//cursor.Index--;
+			//cursor.EmitDelegate<Action<bool>>((res) => inRound = res);
+
+
+			cursor.Index -= 3;
+
+			//test = cursor.Prev.OpCode.ToString() + " " + cursor.Prev.Operand?.ToString() + "; " + test;
+			//cursor.Index--;
+			//test = cursor.Prev.OpCode.ToString() + " " + cursor.Prev.Operand?.ToString() + "; " + test;
+			//cursor.Index--;
+			//test = cursor.Prev.OpCode.ToString() + " " + cursor.Prev.Operand?.ToString() + "; " + test;
+			//cursor.Index--;
 
 			cursor.EmitDelegate<Action>(() => { inRound = true; });
+			//test = cursor.Prev.OpCode.ToString() + " " + cursor.Prev.Operand?.ToString() + "; " + test;
+			//cursor.Index--;
+			//test = cursor.Prev.OpCode.ToString() + " " + cursor.Prev.Operand?.ToString() + "; " + test;
+			//cursor.Index--;
+
+
 		}
 
 		public static void onMadhuntStopRound(ILContext il)
@@ -53,7 +150,7 @@ namespace Celeste.Mod.EmoteMod
 
 			// this one has no extra steps so just yea
 			cursor.Index = 0;
-			cursor.EmitDelegate<Action>(() => { inRound = false; });
+			cursor.EmitDelegate<Action>(() => { inRound = false; EmoteModMain.echo(inRound.ToString()); });
 		}
 
 
@@ -61,8 +158,8 @@ namespace Celeste.Mod.EmoteMod
 		{
 			if (madhuntLoaded)
 			{
-				onMadhuntStartRoundHook.Dispose();
-				onMadhuntStopRoundHook.Dispose ();
+				//onMadhuntStartRoundHook.Dispose();
+				//onMadhuntStopRoundHook.Dispose ();
 			}
 		}
 	}
