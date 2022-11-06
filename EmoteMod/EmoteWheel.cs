979a30c72bcf13bad40810ae3f596cbfe3dcb688
DesantEu <wazzap203203@list.ru>
2022-11-06 20:03:31 +0100

    looking nice

--- EmoteMod/EmoteWheel.cs
+++ EmoteMod/EmoteWheel.cs
@@ -9,293 +9,362 @@ using System.Linq;
 
 namespace Celeste.Mod.EmoteMod
 {
-    // TODO: This is taken mostly as is from GhostNet and can be improved.
-    public class EmoteWheel : Entity
-    {
+	// TODO: This is taken mostly as is from GhostNet and can be improved.
+	public class EmoteWheel : Entity
+	{
 
-        public Entity Tracking;
+		public Entity Tracking;
 
-        public float Alpha = 1f;
+		public float Alpha = 1f;
 
-        protected float time = 0f;
+		protected float time = 0f;
 
-        public bool Shown = false;
-        protected bool popupShown = false;
-        protected float popupTime = 100f;
-        protected bool timeRateSet = false;
+		public bool Shown = false;
+		protected bool popupShown = false;
+		protected float popupTime = 100f;
+		protected bool timeRateSet = false;
 
-        public HashSet<string> TimeRateSkip = new HashSet<string>();
-        public float timeSkipForcedDelay = -1f;
-        public bool ForceSetTimeRate;
+		public HashSet<string> TimeRateSkip = new HashSet<string>();
+		public float timeSkipForcedDelay = -1f;
+		public bool ForceSetTimeRate;
 
-        public float Angle = 0f;
+		public float Angle = 0f;
 
-        public int Selected = -1;
-        protected int PrevSelected;
-        protected float selectedTime = 0f;
+		public int Selected = -1;
+		protected int PrevSelected;
+		protected float selectedTime = 0f;
 
-        public MTexture BG = GFX.Gui["emotemod/iconwheel/bg"];
-        public MTexture Line = GFX.Gui["emotemod/iconwheel/line"];
-        public MTexture Indicator = GFX.Gui["emotemod/iconwheel/indicator"];
+		//public MTexture BG = GFX.Gui["emotemod/iconwheel/bg"];
+		//public MTexture Line = GFX.Gui["emotemod/iconwheel/line"];
+		//public MTexture Indicator = GFX.Gui["emotemod/iconwheel/indicator"];
+		public MTexture Petal = GFX.Gui["emotemod/emotewheel/petalv2_short"];
+		public MTexture PetalBottom = GFX.Gui["emotemod/emotewheel/petalv2_short_bottom"];
 
-        public Color TextSelectColorA = Calc.HexToColor("84FF54");
-        public Color TextSelectColorB = Calc.HexToColor("FCFF59");
+		public Color TextSelectColorA = Calc.HexToColor("84FF54");
+		public Color TextSelectColorB = Calc.HexToColor("FCFF59");
 
 
 
-        public EmoteWheel(Entity tracking)
-            : base(Vector2.Zero)
-        {
-            Tracking = tracking;
+		public EmoteWheel(Entity tracking)
+			: base(Vector2.Zero)
+		{
+			Tracking = tracking;
 
-            Tag = TagsExt.SubHUD;
-            Depth = -1;
-        }
+			Tag = TagsExt.SubHUD;
+			Depth = -1;
+		}
 
-        public override void Update()
-        {
-            if (TimeRateSkip.Contains("EmptySpaceHeart") &&
-                timeSkipForcedDelay <= 0f &&
-                Engine.Scene is Level l && !l.InCutscene)
-            {
-                TimeRateSkip.Remove("EmptySpaceHeart");
-            }
+		public override void Update()
+		{
+			if (TimeRateSkip.Contains("EmptySpaceHeart") &&
+				timeSkipForcedDelay <= 0f &&
+				Engine.Scene is Level l && !l.InCutscene)
+			{
+				TimeRateSkip.Remove("EmptySpaceHeart");
+			}
 
-            if (timeSkipForcedDelay >= 0f)
-            {
-                timeSkipForcedDelay -= Engine.RawDeltaTime;
-            }
+			if (timeSkipForcedDelay >= 0f)
+			{
+				timeSkipForcedDelay -= Engine.RawDeltaTime;
+			}
 
-            // TimeRate check is for Prologue Dash prompt freeze
-            if (Engine.TimeRate > 0.05f && (TimeRateSkip.Count == 0 || ForceSetTimeRate))
-            {
-                if (Shown && !timeRateSet)
-                {
-                    Engine.TimeRate = 0.25f;
-                    timeRateSet = true;
+			// TimeRate check is for Prologue Dash prompt freeze
+			if (Engine.TimeRate > 0.05f && (TimeRateSkip.Count == 0 || ForceSetTimeRate))
+			{
+				if (Shown && !timeRateSet)
+				{
+					Engine.TimeRate = 0.25f;
+					timeRateSet = true;
 
-                }
-                else if (!Shown && timeRateSet)
-                {
-                    Engine.TimeRate = 1f;
-                    timeRateSet = false;
-                }
-            }
+				}
+				else if (!Shown && timeRateSet)
+				{
+					Engine.TimeRate = 1f;
+					timeRateSet = false;
+				}
+			}
 
-            base.Update();
+			base.Update();
 
-            if (Tracking == null || Tracking?.Scene != Scene)
-                RemoveSelf();
-        }
+			if (Tracking == null || Tracking?.Scene != Scene)
+				RemoveSelf();
+		}
 
-        public override void Render()
-        {
-            base.Render();
+		public override void Render()
+		{
+			base.Render();
 
-            string[] emotes = {
-                EmoteModMain.Settings.emote0,
-                EmoteModMain.Settings.emote1,
-                EmoteModMain.Settings.emote2,
-                EmoteModMain.Settings.emote3,
-                EmoteModMain.Settings.emote4,
-                EmoteModMain.Settings.emote5,
-                EmoteModMain.Settings.emote6,
-                EmoteModMain.Settings.emote7,
-                EmoteModMain.Settings.emote8,
-                EmoteModMain.Settings.emote9
-            };
+			string[] emotes = {
+				EmoteModMain.Settings.emote0,
+				EmoteModMain.Settings.emote1,
+				EmoteModMain.Settings.emote2,
+				EmoteModMain.Settings.emote3,
+				EmoteModMain.Settings.emote4,
+				EmoteModMain.Settings.emote5,
+				EmoteModMain.Settings.emote6,
+				EmoteModMain.Settings.emote7,
+				EmoteModMain.Settings.emote8,
+				EmoteModMain.Settings.emote9
+			};
 
-            // Update can halt in the pause menu.
+			// Update can halt in the pause menu.
 
-            if (Shown)
-            {
-                Angle = EmoteWheelModule.JoystickEmoteWheel.Value.Angle();
-                float angle = (float)((Angle + Math.PI * 2f) % (Math.PI * 2f));
-                float start = (-0.5f / emotes.Length) * 2f * (float)Math.PI;
-                if (2f * (float)Math.PI + start < angle)
-                {
-                    // Angle should be start < angle < 0, but is (TAU + start) < angle < TAU
-                    angle -= 2f * (float)Math.PI;
-                }
-                for (int i = 0; i < emotes.Length; i++)
-                {
-                    float min = ((i - 0.5f) / emotes.Length) * 2f * (float)Math.PI;
-                    float max = ((i + 0.5f) / emotes.Length) * 2f * (float)Math.PI;
-                    if (min <= angle && angle <= max)
-                    {
-                        Selected = i;
-                        break;
-                    }
-                }
-            }
+			if (Shown)
+			{
+				Angle = EmoteWheelModule.JoystickEmoteWheel.Value.Angle();
+				float angle = (float)((Angle + Math.PI * 2f) % (Math.PI * 2f));
+				float start = (-0.5f / emotes.Length) * 2f * (float)Math.PI;
+				if (2f * (float)Math.PI + start < angle)
+				{
+					// Angle should be start < angle < 0, but is (TAU + start) < angle < TAU
+					angle -= 2f * (float)Math.PI;
+				}
+				for (int i = 0; i < emotes.Length; i++)
+				{
+					float min = ((i - 0.5f) / emotes.Length) * 2f * (float)Math.PI;
+					float max = ((i + 0.5f) / emotes.Length) * 2f * (float)Math.PI;
+					if (min <= angle && angle <= max)
+					{
+						Selected = i;
+						break;
+					}
+				}
+			}
 
-            time += Engine.RawDeltaTime;
+			time += Engine.RawDeltaTime;
 
-            if (!Shown)
-            {
-                Selected = -1;
-            }
-            selectedTime += Engine.RawDeltaTime;
-            if (PrevSelected != Selected)
-            {
-                selectedTime = 0f;
-                PrevSelected = Selected;
-            }
+			if (!Shown)
+			{
+				Selected = -1;
+			}
+			selectedTime += Engine.RawDeltaTime;
+			if (PrevSelected != Selected)
+			{
+				selectedTime = 0f;
+				PrevSelected = Selected;
+			}
 
-            float popupAlpha;
-            float popupScale;
+			float popupAlpha;
+			float popupScale;
 
-            popupTime += Engine.RawDeltaTime;
-            if (Shown && !popupShown)
-            {
-                popupTime = 0f;
-            }
-            else if ((Shown && popupTime > 1f) ||
-                (!Shown && popupTime < 1f))
-            {
-                popupTime = 1f;
-            }
-            popupShown = Shown;
+			popupTime += Engine.RawDeltaTime;
+			if (Shown && !popupShown)
+			{
+				popupTime = 0f;
+			}
+			else if ((Shown && popupTime > 1f) ||
+				(!Shown && popupTime < 1f))
+			{
+				popupTime = 1f;
+			}
+			popupShown = Shown;
 
-            if (popupTime < 0.1f)
-            {
-                float t = popupTime / 0.1f;
-                // Pop in.
-                popupAlpha = Ease.CubeOut(t);
-                popupScale = Ease.ElasticOut(t);
+			if (popupTime < 0.1f)
+			{
+				float t = popupTime / 0.1f;
+				// Pop in.
+				popupAlpha = Ease.CubeOut(t);
+				popupScale = Ease.ElasticOut(t);
 
-            }
-            else if (popupTime < 1f)
-            {
-                // Stay.
-                popupAlpha = 1f;
-                popupScale = 1f;
+			}
+			else if (popupTime < 1f)
+			{
+				// Stay.
+				popupAlpha = 1f;
+				popupScale = 1f;
 
-            }
-            else
-            {
-                float t = (popupTime - 1f) / 0.2f;
-                // Fade out.
-                popupAlpha = 1f - Ease.CubeIn(t);
-                popupScale = 1f - 0.2f * Ease.CubeIn(t);
-            }
+			}
+			else
+			{
+				float t = (popupTime - 1f) / 0.2f;
+				// Fade out.
+				popupAlpha = 1f - Ease.CubeIn(t);
+				popupScale = 1f - 0.2f * Ease.CubeIn(t);
+			}
 
-            float alpha = Alpha * popupAlpha;
+			float alpha = Alpha * popupAlpha;
 
-            if (alpha <= 0f)
-                return;
+			if (alpha <= 0f)
+				return;
 
-            if (Tracking == null)
-                return;
+			if (Tracking == null)
+				return;
 
-            Level level = SceneAs<Level>();
-            if (level == null)
-                return;
+			Level level = SceneAs<Level>();
+			if (level == null)
+				return;
 
-            popupScale *= level.GetScreenScale();
+			popupScale *= level.GetScreenScale();
 
-            Vector2 pos = Tracking.Position;
-            pos.Y -= 8f;
+			Vector2 pos = Tracking.Position;
+			pos.Y -= 8f;
 
-            pos = level.WorldToScreen(pos);
+			pos = level.WorldToScreen(pos);
 
-            float radius = BG.Width * 0.5f * 0.75f * popupScale;
+			float radius = Petal.Width * 0.5f * 0.75f * popupScale;
 
-            pos = pos.Clamp(
-                0f + radius, 0f + radius,
-                1920f - radius, 1080f - radius
-            );
+			pos = pos.Clamp(
+				0f + radius, 0f + radius,
+				1920f - radius, 1080f - radius
+			);
 
-            BG.DrawCentered(
-                pos,
-                Color.White * alpha * alpha * alpha,
-                Vector2.One * popupScale
-            );
+			float selScaleScale = 0.67f;
 
-            Indicator.DrawCentered(
-                pos,
-                Color.White * alpha * alpha * alpha,
-                Vector2.One * popupScale,
-                Angle
-            );
+			// draw petals
+			for (int i = 0; i < emotes.Length; i++)
+			{
+				float selScale = Selected == i ? 128f * selScaleScale : 64f;
 
-            float selectedScale = 1.2f - 0.2f * Calc.Clamp(Ease.CubeOut(selectedTime / 0.1f), 0f, 1f) + (float)Math.Sin(time * 1.8f) * 0.05f;
+				float rot = ((i /*+ 0.5f*/) / 10f) * 2f * (float)Math.PI;
 
-            for (int i = 0; i < emotes.Length; i++)
-            {
-                Line.DrawCentered(
-                    pos,
-                    Color.White * alpha * alpha * alpha,
-                    Vector2.One * popupScale,
-                    ((i + 0.5f) / emotes.Length) * 2f * (float)Math.PI
-                );
+				Petal.DrawCentered(
+				pos + new Vector2((float)Math.Cos(rot), (float)Math.Sin(rot)) * selScale,
+				Color.White * alpha * alpha * alpha,
+				Vector2.One * popupScale * (Selected == i ? 1f * selScaleScale : 0.5f),
+				rot
+				);
 
-                string emote = emotes[i];
-                if (string.IsNullOrEmpty(emote))
-                    continue;
+				if (Selected == i)
+				{
+					PetalBottom.DrawCentered(
+					pos + new Vector2((float)Math.Cos(rot), (float)Math.Sin(rot)) * 64f,
+					Color.White * alpha * alpha * alpha,
+					Vector2.One * popupScale * 0.5f,
+					rot
+					);
+				}
+			}
 
-                float a = (i / (float)emotes.Length) * 2f * (float)Math.PI;
-                Vector2 emotePos = pos + new Vector2(
-                    (float)Math.Cos(a),
-                    (float)Math.Sin(a)
-                ) * radius;
+			// draw emotes
+			float selectedScale = 1.2f - 0.2f * Calc.Clamp(Ease.CubeOut(selectedTime / 0.1f), 0f, 1f) + (float)Math.Sin(time * 1.8f) * 0.05f * 2f * selScaleScale;
 
-                MTexture[] tanim = getTextureByName(emote);
-                MTexture icon = tanim[tanim.Length > 2 ? tanim.Length/2 : 0];
-                if (icon == null)
-                    continue;
+			for (int i = 0; i < emotes.Length; i++)
+			{
 
-                Vector2 iconSize = new Vector2(icon.Width, icon.Height);
-                float iconScale = (Math.Max(icon.Width, icon.Height) / Math.Max(iconSize.X, iconSize.Y)) * 2.24f * popupScale;
+				string emote = emotes[i];
+				if (string.IsNullOrEmpty(emote))
+					continue;
 
-                emotePos.Y -= (iconScale * iconSize.Y) / 3f;
+				float a = (i / (float)emotes.Length) * 2f * (float)Math.PI;
+				Vector2 emotePos = pos + new Vector2(
+					(float)Math.Cos(a),
+					(float)Math.Sin(a)
+				) * radius * (Selected == i ? 2f * selScaleScale : 1f);
 
-                icon.DrawCentered(
-                    emotePos,
-                    Color.White * (Selected == i ? (Calc.BetweenInterval(selectedTime, 0.1f) ? 0.9f : 1f) : 0.7f) * alpha,
-                    Vector2.One * (Selected == i ? selectedScale : 1f) * iconScale
-                );
+				MTexture[] tanim = getTextureByName(emote);
+				MTexture icon = tanim[tanim.Length > 2 ? tanim.Length / 2 : 0];
+				if (icon == null)
+					continue;
 
-            }
-        }
+				Vector2 iconSize = new Vector2(icon.Width, icon.Height);
+				float iconScale = (Math.Max(icon.Width, icon.Height) / Math.Max(iconSize.X, iconSize.Y)) * /*2.24f*/ 2.5f * popupScale;
 
-        private MTexture[] getTextureByName(string animation)
-        {
+				//emotePos *= (Selected == i ? 1.2f : 1f);
+				emotePos.Y -= (iconScale * iconSize.Y) / 3f;
+
+				icon.DrawCentered(
+					emotePos,
+					Color.White * (Selected == i ? (Calc.BetweenInterval(selectedTime, 0.1f) ? 0.9f : 1f) : 1f) * alpha,
+					Vector2.One * (Selected == i ? selectedScale * 2f * selScaleScale : 1f) * iconScale
+				);
+			}
+			//Petal.DrawCentered(
+			//             pos + new Vector2(128, 0),
+			//             Color.White * alpha * alpha * alpha,
+			//             Vector2.One * popupScale
+			//             );
+
+			//BG.DrawCentered(
+			//    pos,
+			//    Color.White * alpha * alpha * alpha,
+			//    Vector2.One * popupScale
+			//);
+
+			//Indicator.DrawCentered(
+			//    pos,
+			//    Color.White * alpha * alpha * alpha,
+			//    Vector2.One * popupScale,
+			//    Angle
+			//);
+
+			//float selectedScale = 1.2f - 0.2f * Calc.Clamp(Ease.CubeOut(selectedTime / 0.1f), 0f, 1f) + (float)Math.Sin(time * 1.8f) * 0.05f;
+
+			//for (int i = 0; i < emotes.Length; i++)
+			//{
+			//    Line.DrawCentered(
+			//        pos,
+			//        Color.White * alpha * alpha * alpha,
+			//        Vector2.One * popupScale,
+			//        ((i + 0.5f) / emotes.Length) * 2f * (float)Math.PI
+			//    );
+
+			//    string emote = emotes[i];
+			//    if (string.IsNullOrEmpty(emote))
+			//        continue;
+
+			//    float a = (i / (float)emotes.Length) * 2f * (float)Math.PI;
+			//    Vector2 emotePos = pos + new Vector2(
+			//        (float)Math.Cos(a),
+			//        (float)Math.Sin(a)
+			//    ) * radius;
+
+			//    MTexture[] tanim = getTextureByName(emote);
+			//    MTexture icon = tanim[tanim.Length > 2 ? tanim.Length / 2 : 0];
+			//    if (icon == null)
+			//        continue;
+
+			//    Vector2 iconSize = new Vector2(icon.Width, icon.Height);
+			//    float iconScale = (Math.Max(icon.Width, icon.Height) / Math.Max(iconSize.X, iconSize.Y)) * 2.24f * popupScale;
+
+			//    emotePos.Y -= (iconScale * iconSize.Y) / 3f;
+
+			//    icon.DrawCentered(
+			//        emotePos,
+			//        Color.White * (Selected == i ? (Calc.BetweenInterval(selectedTime, 0.1f) ? 0.9f : 1f) : 0.7f) * alpha,
+			//        Vector2.One * (Selected == i ? selectedScale : 1f) * iconScale
+			//    );
+
+			//}
+		}
+
+		private MTexture[] getTextureByName(string animation)
+		{
 			Dictionary<string, Sprite.Animation> madeline_bp = GFX.SpriteBank.SpriteData["player"].Sprite.Animations;
 			Dictionary<string, Sprite.Animation> madeline_no_bp = GFX.SpriteBank.SpriteData["player_no_backpack"].Sprite.Animations;
 			Dictionary<string, Sprite.Animation> madeline_badeline = GFX.SpriteBank.SpriteData["player_badeline"].Sprite.Animations;
 			Dictionary<string, Sprite.Animation> badeline = GFX.SpriteBank.SpriteData["badeline"].Sprite.Animations;
 
-            // b
-            if (animation == "b")
-            {
-                return getTextureByName("spin");
-            }
+			// b
+			if (animation == "b")
+			{
+				return getTextureByName("spin");
+			}
 
-            // this sucks but idk
-            if (madeline_no_bp.Keys.Contains(animation, StringComparer.OrdinalIgnoreCase))
-            {
-                return madeline_no_bp[animation].Frames;
-            }
-            else if (madeline_bp.Keys.Contains(animation, StringComparer.OrdinalIgnoreCase))
-            {
-                return madeline_bp[animation].Frames;
-            }
-            else if (badeline.Keys.Contains(animation, StringComparer.OrdinalIgnoreCase))
-            {
-                return badeline[animation].Frames;
-            }
+			// this sucks but idk
+			if (madeline_no_bp.Keys.Contains(animation, StringComparer.OrdinalIgnoreCase))
+			{
+				return madeline_no_bp[animation].Frames;
+			}
+			else if (madeline_bp.Keys.Contains(animation, StringComparer.OrdinalIgnoreCase))
+			{
+				return madeline_bp[animation].Frames;
+			}
+			else if (badeline.Keys.Contains(animation, StringComparer.OrdinalIgnoreCase))
+			{
+				return badeline[animation].Frames;
+			}
 
-            else if (findCustomEmote(animation) != null)
-            {
-                return findCustomEmote(animation);
-            }
+			else if (findCustomEmote(animation) != null)
+			{
+				return findCustomEmote(animation);
+			}
 
-            else {
-                EmoteModMain.echo($"EMOTEMOD ERROR: Could not find '{animation}'");
+			else
+			{
+				EmoteModMain.echo($"EMOTEMOD ERROR: Could not find '{animation}'");
 
-                return getTextureByName("player_idle");
-            }
+				return getTextureByName("player_idle");
+			}
 		}
 
 		private MTexture[] findCustomEmote(string animation)
