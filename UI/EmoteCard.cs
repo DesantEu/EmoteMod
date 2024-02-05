96bb060a9cd192ddc51a4c9fb2a8b38d241cfcb0
DesantEu <wazzap203203@list.ru>
2024-02-05 17:53:33 +0100

    changed textures to in-game, first attempts at keybinds (crashes), tried to render hair(does not show up)

--- UI/EmoteCard.cs
+++ UI/EmoteCard.cs
@@ -4,6 +4,8 @@ using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Input;
 using System.Collections;
 using System;
+using System.Reflection;
+using System.Linq;
 
 namespace Celeste.Mod.EmoteMod
 {
@@ -11,32 +13,42 @@ namespace Celeste.Mod.EmoteMod
     {
         None, Animation, Spritebank, Keys, Save, Cancel, Delete
     }
-    internal class EmoteCard : Entity
+    public class EmoteCard : Entity
     {
-        MTexture card = GFX.Gui["emotemod/card"];
-        MTexture ticket = GFX.Gui["emotemod/ticket"];
+        MTexture card = MTN.FileSelect.Textures["card"];
+        // MTexture card = GFX.Gui["emotemod/card"];
+        // MTexture ticket = GFX.Gui["emotemod/ticket"];
+        MTexture ticket = MTN.FileSelect.Textures["ticket"];
+
+        // SpriteGrid gallery;
 
         float animation_scale = 10f;
         public EmoteEntry emote;
+        public EmoteEntry old_emote;
 
         PlayerSprite sprite;
+        PlayerHair hair;
         public bool Focused;
         public bool Selected;
         public bool Opened;
+        bool changesMade;
+        bool isReadingKey = false;
         Butt atButton = Butt.None;
         Wiggler wiggler = Wiggler.Create(0.25f, 3f);
+        Wiggler save_cancel_wiggler = Wiggler.Create(0.25f, 3f);
 
         float edit_scale, anim_name_scale, spritebank_scale, save_scale, cancel_scale, delete_scale;
 
         public OuiEmoteConfigMenu parent;
         Color flashing_color => !Settings.Instance.DisableFlashes && !this.Scene.BetweenInterval(0.1f) ? TextMenu.HighlightColorB : TextMenu.HighlightColorA;
 
-        // int width, height;
 
         Vector2 ticket_shift, card_shift;
         bool drawTicketOnTop = false;
 
         public IEnumerator coro;
+        List<MTexture> keytextures = new();
+        float total_keys_width;
 
         public override void Render()
         {
@@ -45,6 +57,7 @@ namespace Celeste.Mod.EmoteMod
             if (!Visible)
                 return;
 
+            // bye bye blur
             HudRenderer.EndRender();
             HudRenderer.BeginRender(null, Microsoft.Xna.Framework.Graphics.SamplerState.PointClamp);
 
@@ -56,7 +69,7 @@ namespace Celeste.Mod.EmoteMod
                 renderTicket();
 
 
-            // EmoteModModule.echo($"lol rendering at {X}:{Y}");
+            // hello blur :3
             HudRenderer.EndRender();
             HudRenderer.BeginRender();
 
@@ -70,12 +83,13 @@ namespace Celeste.Mod.EmoteMod
             ActiveFont.DrawOutline("Edit", Position + ticket_shift + new Vector2(card.Width / 4, ActiveFont.LineHeight / 2),
                     Vector2.One, new Vector2(1, Math.Clamp(edit_scale, 0, 1)),
                     Color.White, 2f, Color.Black);
-            // save cancel delete
 
-            ActiveFont.DrawOutline("Save", Position + ticket_shift + new Vector2(atButton == Butt.Save ? wiggler.Value * 8f : 0, -70),
+            // save cancel delete
+            // literally unreadable
+            ActiveFont.DrawOutline("Save", Position + ticket_shift + new Vector2((atButton == Butt.Save ? wiggler.Value * 8f : 0) + save_cancel_wiggler.Value * 8f, -70),
                     Vector2.One * 0.5f, new Vector2(1, save_scale),
                     atButton == Butt.Save ? flashing_color : Color.White, 2f, Color.Black);
-            ActiveFont.DrawOutline("Cancel", Position + ticket_shift + new Vector2(atButton == Butt.Cancel ? wiggler.Value * 8f : 0, 0),
+            ActiveFont.DrawOutline("Cancel", Position + ticket_shift + new Vector2((atButton == Butt.Cancel ? wiggler.Value * 8f : 0) + save_cancel_wiggler.Value * 8f, 0),
                     Vector2.One * 0.5f, new Vector2(1, cancel_scale),
                     atButton == Butt.Cancel ? flashing_color : Color.White, 2f, Color.Black);
             ActiveFont.DrawOutline("Delete", Position + ticket_shift + new Vector2(atButton == Butt.Delete ? wiggler.Value * 8f : 0, 70),
@@ -87,73 +101,112 @@ namespace Celeste.Mod.EmoteMod
         {
             card.DrawCentered(Position + card_shift);
 
-            ActiveFont.DrawOutline(emote.animation, Position + card_shift + new Vector2(card.Width / 6, -30) + new Vector2(atButton == Butt.Animation ? wiggler.Value * 8f : 0, 0)
+            // emote name
+            ActiveFont.DrawOutline(emote.GetInfo().animation, Position + card_shift + new Vector2(card.Width / 6, -30) + new Vector2(atButton == Butt.Animation ? wiggler.Value * 8f : 0, 0)
                     , new Vector2(0.5f, 1), new Vector2(1, Math.Max(anim_name_scale, 0)),
                     atButton == Butt.Animation ? flashing_color : Color.White, 2f, Color.Black);
-            ActiveFont.DrawOutline(emote.animation, Position + card_shift + new Vector2(card.Width / 6, 10) + new Vector2(atButton == Butt.Spritebank ? wiggler.Value * 8f : 0, 0)
+            // spritebank
+            string sb = emote.GetInfo().isCustom ? emote.GetInfo().spritebank : "Default";
+            ActiveFont.DrawOutline(sb, Position + card_shift + new Vector2(card.Width / 6, 10) + new Vector2(atButton == Butt.Spritebank ? wiggler.Value * 8f : 0, 0)
                     , new Vector2(0.5f, 1), new Vector2(1, Math.Max(spritebank_scale, 0)) * 0.8f,
                     atButton == Butt.Spritebank ? flashing_color : Color.White, 2f, Color.Black);
 
-            ActiveFont.Draw(emote.animation, Position + card_shift + new Vector2(card.Width / 6, -30)
+            // same but with outline
+            ActiveFont.Draw(emote.GetInfo().animation, Position + card_shift + new Vector2(card.Width / 6, -30)
                     , new Vector2(0.5f, 1), new Vector2(1, Math.Max(-anim_name_scale, 0)), Color.Black * 0.8f);
-            ActiveFont.Draw(emote.animation, Position + card_shift + new Vector2(card.Width / 6, 10)
+            ActiveFont.Draw(sb, Position + card_shift + new Vector2(card.Width / 6, 10)
                     , new Vector2(0.5f, 1), new Vector2(1, Math.Max(-spritebank_scale, 0)) * 0.8f, Color.Black * 0.6f);
 
 
+            // keybinds
             Vector2 keys_center = new(card.Width / 6, 50);
-            if (emote.bind.Keys.Count == 1)
+            // draw one
+            // if (emote.bind.Keys.Count == 1)
+            // {
+            //     MTexture tex = GFX.Gui[$"controls/keyboard/{emote.bind.Keys[0]}"];
+            //     tex.DrawOutlineCentered(Position + card_shift + keys_center + new Vector2(atButton == Butt.Keys ? wiggler.Value * 8f : 0, 0)
+            //             , atButton == Butt.Keys ? flashing_color : Color.White);
+            // }
+            // draw an array centered
+            if (keytextures.Count > 0)
+            // if (emote.bind.Keys.Count > 0)
             {
-                MTexture tex = GFX.Gui[$"controls/keyboard/{emote.bind.Keys[0]}"];
-                tex.DrawOutlineCentered(Position + card_shift + keys_center + new Vector2(atButton == Butt.Keys ? wiggler.Value * 8f : 0, 0)
-                        , atButton == Butt.Keys ? flashing_color : Color.White);
-            }
-            else if (emote.bind.Keys.Count > 1)
-            {
-                List<MTexture> keytextures = new();
-                float totalwidth = 0;
-
-                foreach (Keys k in emote.bind.Keys)
-                {
-                    MTexture tex = GFX.Gui[$"controls/keyboard/{k}"];
-                    keytextures.Add(tex);
-                    totalwidth += tex.Width;
-                    // tex.DrawOutlineCentered(Position + card_shift + new Vector2(card.Width / 6, 50));
-                }
-                float half = totalwidth / 2;
+                // List<MTexture> keytextures = new();
+                // float totalwidth = 0;
+                //
+                // foreach (Keys k in emote.bind.Keys)
+                // {
+                //     MTexture tex = GFX.Gui[$"controls/keyboard/{k}"];
+                //     keytextures.Add(tex);
+                //     totalwidth += tex.Width;
+                //     // tex.DrawOutlineCentered(Position + card_shift + new Vector2(card.Width / 6, 50));
+                // }
+                float half = total_keys_width / 2;
+                float used = 0;
                 foreach (MTexture t in keytextures)
                 {
-                    totalwidth -= t.Width;
-                    t.DrawOutlineCentered(Position + card_shift + keys_center + new Vector2(half - totalwidth - t.Width / 2, 0) + new Vector2(atButton == Butt.Keys ? wiggler.Value * 8f : 0, 0)
+                    used += t.Width;
 
+                    t.DrawOutlineCentered(Position + card_shift + keys_center
+                            + new Vector2(half - total_keys_width + used - t.Width / 2, 0)
+                            + new Vector2(atButton == Butt.Keys ? wiggler.Value * 8f : 0, 0)
                             , atButton == Butt.Keys ? flashing_color : Color.White);
                 }
             }
+            // or draw text if none
             else
             {
                 ActiveFont.Draw("Add keys", Position + card_shift + keys_center + new Vector2(atButton == Butt.Keys ? wiggler.Value * 8f : 0, 0)
 
-                        , Vector2.One, new Vector2(0.5f, 0.5f), atButton == Butt.Keys ? flashing_color : Color.White);
+                        , new Vector2(0.5f, 0.5f), Vector2.One, atButton == Butt.Keys ? flashing_color : Color.White);
             }
             sprite.Render();
         }
 
+        public void RegenerateKeyTextures(bool controller = false)
+        {
+            keytextures = new();
+            total_keys_width = 0;
+
+            foreach (Keys k in emote.bind.Keys)
+            {
+                MTexture tex = GFX.Gui[$"controls/keyboard/{k}"];
+                keytextures.Add(tex);
+                total_keys_width += tex.Width;
+                // tex.DrawOutlineCentered(Position + card_shift + new Vector2(card.Width / 6, 50));
+            }
+        }
+
         public override void Update()
         {
+            // if (Focused)
+            //     EmoteModModule.echo($"old keys:{old_emote.bind.Keys.Count}, new:{emote.bind.Keys.Count}");
             base.Update();
+            // do we need to do this?
             sprite.Position = Position + new Vector2(-card.Width / 4, card.Height / 4) + card_shift;
             sprite.Update();
 
             wiggler.Update();
+            save_cancel_wiggler.Update();
 
             if (coro != null)
                 coro.MoveNext();
 
+            // inputs stuff
             if (Focused)
             {
+                // try exit
                 if (Input.MenuCancel.Pressed)
                 {
-                    coro = OnClose();
+                    if (changesMade)
+                    {
+                        // save_cancel_wiggle = true;
+                        save_cancel_wiggler.Start();
+                    }
+                    else
+                        coro = OnClose();
                 }
+                // down
                 else if (Input.MenuDown.Pressed)
                 {
                     if (atButton == Butt.Delete)
@@ -162,6 +215,7 @@ namespace Celeste.Mod.EmoteMod
                     atButton++;
                     wiggler.Start();
                 }
+                // up
                 else if (Input.MenuUp.Pressed)
                 {
                     atButton--;
@@ -169,6 +223,48 @@ namespace Celeste.Mod.EmoteMod
                         atButton = Butt.Delete;
                     wiggler.Start();
 
+                }
+                // clear keys
+                else if (Input.MenuJournal.Pressed)
+                {
+                    if (atButton == Butt.Keys)
+                    {
+                        changesMade = true;
+                        emote.bind.Keys.Clear();
+                        RegenerateKeyTextures();
+                    }
+                }
+                // confirm
+                else if (Input.MenuConfirm.Pressed)
+                {
+                    // save
+                    if (atButton == Butt.Save)
+                    {
+                        int index = EmoteModModule.Settings.Emotes.IndexOf(old_emote);
+                        EmoteModModule.Settings.Emotes[index] = emote;
+                        coro = OnClose();
+                    }
+                    // cancel
+                    else if (atButton == Butt.Cancel)
+                    {
+                        changesMade = false;
+                        emote = old_emote;
+                        coro = OnClose();
+                    }
+                    // add keys/buttons
+                    else if (atButton == Butt.Keys)
+                    {
+                        coro = OnAddBind();
+                    }
+                    // change spritemode
+                    else if (atButton == Butt.Spritebank)
+                    {
+                        Focused = false;
+                        atButton = Butt.None;
+
+                        // gallery = new SpriteGrid(emote.GetInfo().spritebank);
+
+                    }
                 }
             }
 
@@ -194,6 +290,65 @@ namespace Celeste.Mod.EmoteMod
         }
 
         #region animations
+        public IEnumerator OnAddBind()
+        {
+            // todo rewrite this pretty much
+            // consider mouse and gamepad
+            Focused = false;
+            isReadingKey = true;
+            // this feels dumb idk
+            bool confirm_skip = true;
+
+            MTexture new_button_placeholder = GFX.Gui["controls/keyboard/OemQuestion"];
+            keytextures.Add(new_button_placeholder);
+            total_keys_width += new_button_placeholder.Width;
+
+            List<Keys> menu_confirm_keys = Settings.Instance.Confirm.Keyboard;
+            List<Buttons> menu_confirm_buttons = Settings.Instance.Confirm.Controller;
+            List<MInput.MouseData.MouseButtons> menu_confirm_mouse = Settings.Instance.Confirm.Mouse;
+
+            for (float d = 0; d < 5f; d += Engine.DeltaTime)
+            {
+                // this feels very dumb but cheching input.menuconfirm doesnt work idk
+                if (menu_confirm_keys.Intersect(Monocle.MInput.Keyboard.CurrentState.GetPressedKeys()).Count() > 0 && confirm_skip)
+                    yield return null;
+                else
+                {
+                    confirm_skip = false;
+                    // Monocle.MInput.Keyboard.CurrentState.GetPressedKeys().
+                    if (MInput.Keyboard.HasAnyInput())
+                    {
+                        changesMade = true;
+                        Keys pressed_key = MInput.Keyboard.CurrentState.GetPressedKeys()[0];
+
+                        if (emote.bind.Keys.Contains(pressed_key))
+                            emote.bind.Keys.Remove(pressed_key);
+                        else
+                            emote.bind.Keys.Add(pressed_key);
+                        break;
+                    }
+                    else if (MInput.GamePads[Input.Gamepad].HasAnyInput())
+                    {
+
+                    }
+                    // else if ()
+                    //     yield return null;
+                }
+            }
+
+            // Input.MenuConfirm.ConsumePress();
+            // Input.MenuJournal.ConsumePress();
+            // Input.MenuCancel.ConsumePress();
+            // Input.menu
+            // MInput.Mouse.
+            foreach (FieldInfo f in typeof(Input).GetFields().Where(i => i.Name.Contains("Menu")))
+                f.GetType().GetMethod("ConsumePress").Invoke(f, null);
+
+            RegenerateKeyTextures();
+            isReadingKey = false;
+            Focused = true;
+            yield return null;
+        }
         // TODO remake to work from any position (add cs/ts)
         public IEnumerator OnSelect()
         {
@@ -215,6 +370,11 @@ namespace Celeste.Mod.EmoteMod
 
         public IEnumerator OnOpen()
         {
+            // whar
+            old_emote = emote;
+            emote = old_emote.Clone();
+
+
             Vector2 ts = ticket_shift;
             Vector2 cs = card_shift;
             float l = -card.Width / 2;
@@ -338,6 +498,8 @@ namespace Celeste.Mod.EmoteMod
             Tag = Tags.HUD;
 
             this.emote = emote;
+            RegenerateKeyTextures();
+            // On.Monocle.Engine.
 
             edit_scale = 1;
             anim_name_scale = spritebank_scale = -1f;
@@ -347,11 +509,18 @@ namespace Celeste.Mod.EmoteMod
             this.card_shift = Vector2.Zero;
             this.Focused = false;
 
-            sprite = new(PlayerSpriteMode.Madeline);
+            sprite = new(emote.GetInfo().spritemode);
             sprite.Scale = Vector2.One * animation_scale;
 
-            if (sprite.Animations.ContainsKey(emote.animation))
-                sprite.Play(emote.animation);
+            hair = new(sprite) { SimulateMotion = true };
+            hair.Visible = true;
+            // hair.
+
+            sprite.Play(emote.GetInfo().animation);
+
+            // if (sprite.Animations.ContainsKey(emote.animation))
+            //     sprite.Play(emote.animation);
+
         }
 
 
