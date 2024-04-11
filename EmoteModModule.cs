b9d78712c0af0b1f60368e88fcad502e4f614fc2
DesantEu <wazzap203203@list.ru>
2024-04-11 05:19:46 +0200

    moved converter and stuff to where it actually works

--- EmoteModModule.cs
+++ EmoteModModule.cs
@@ -45,35 +45,46 @@ namespace Celeste.Mod.EmoteMod
         // TODO: this looks dumb, find a better way
         public override void LoadSettings()
         {
-            AnimationHelper.Init();
             base.LoadSettings();
-
-            if (!Settings.ConvertedToV2)
-            {
-                Settings.Emotes.Add(new EmoteEntry(Settings.emote0, Settings.button0));
-                Settings.Emotes.Add(new EmoteEntry(Settings.emote1, Settings.button1));
-                Settings.Emotes.Add(new EmoteEntry(Settings.emote2, Settings.button2));
-                Settings.Emotes.Add(new EmoteEntry(Settings.emote3, Settings.button3));
-                Settings.Emotes.Add(new EmoteEntry(Settings.emote4, Settings.button4));
-                Settings.Emotes.Add(new EmoteEntry(Settings.emote5, Settings.button5));
-                Settings.Emotes.Add(new EmoteEntry(Settings.emote6, Settings.button6));
-                Settings.Emotes.Add(new EmoteEntry(Settings.emote7, Settings.button7));
-                Settings.Emotes.Add(new EmoteEntry(Settings.emote8, Settings.button8));
-                Settings.Emotes.Add(new EmoteEntry(Settings.emote9, Settings.button9));
-
-                Settings.ConvertedToV2 = true;
-                SaveSettings();
-            }
         }
 
         public override void SaveSettings()
         {
             base.SaveSettings();
         }
+        public override void Initialize()
+        {
+            base.Initialize();
+
+            AnimationHelper.Init();
+        }
+
+        private void convert_to_v2()
+        {
+            if (!Settings.ConvertedToV2)
+            {
+                Settings.Emotes.Clear();
+                Settings.Emotes.Add(new EmoteEntry(Settings.emote0, Settings.button0, true));
+                Settings.Emotes.Add(new EmoteEntry(Settings.emote1, Settings.button1, true));
+                Settings.Emotes.Add(new EmoteEntry(Settings.emote2, Settings.button2, true));
+                Settings.Emotes.Add(new EmoteEntry(Settings.emote3, Settings.button3, true));
+                Settings.Emotes.Add(new EmoteEntry(Settings.emote4, Settings.button4, true));
+                Settings.Emotes.Add(new EmoteEntry(Settings.emote5, Settings.button5, true));
+                Settings.Emotes.Add(new EmoteEntry(Settings.emote6, Settings.button6, true));
+                Settings.Emotes.Add(new EmoteEntry(Settings.emote7, Settings.button7, true));
+                Settings.Emotes.Add(new EmoteEntry(Settings.emote8, Settings.button8, true));
+                Settings.Emotes.Add(new EmoteEntry(Settings.emote9, Settings.button9, true));
+
+                Settings.ConvertedToV2 = true;
+                SaveSettings();
+            }
+        }
 
         public override void Load()
         {
             anim_by_game = 0; // this tells that base state is no animations
+
+            On.Celeste.GFX.LoadData += on_gfx_load_data;
 
             Gravity.Load();
             Emote.Load();
@@ -88,6 +99,13 @@ namespace Celeste.Mod.EmoteMod
             if (Engine.Scene is Level)
                 foreach (EmoteEntry e in Settings.Emotes)
                     e.RefreshInfo();
+        }
+
+        private void on_gfx_load_data(On.Celeste.GFX.orig_LoadData orig)
+        {
+            orig();
+
+            convert_to_v2();
         }
 
         public override void Unload()
