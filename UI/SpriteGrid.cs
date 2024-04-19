f6407d516ebde0f4d1f0880f5d502f2010e35c07
DesantEu <wazzap203203@list.ru>
2024-04-19 20:42:33 +0200

    got spritegrid aligned, moved elements to a separate file

--- UI/SpriteGrid.cs
+++ UI/SpriteGrid.cs
@@ -14,49 +14,47 @@ namespace Celeste.Mod.EmoteMod
         SpriteBank,
         Emote,
         Done
-
     }
-    public class SpriteGridCell : Entity
-    {
-        PlayerSprite sprite;
-        string text;
-        Vector2 Pos;
-        Wiggler wiggler;
-        bool isSelected;
 
-        public override void Update()
-        {
-            base.Update();
-
-            sprite.Update();
-        }
-
-        public override void Render()
-        {
-            base.Render();
-
-        }
-
-        public SpriteGridCell(EmoteInfo emote, string text, Vector2 pos)
-        {
-            this.Pos = pos;
-            this.text = text;
-
-            this.sprite = new(emote.spritemode);
-            sprite.Play(emote.isCustom ? $"{emote.spritebank}:{emote.animation}" : emote.animation);
-        }
-    }
     public class SpriteGrid : Entity
     {
         IEnumerator coro;
-        float bg_alpha = 0f;
         EmotePickerStage stage;
         EmoteCard parent;
 
+        List<string> customsForRemoval;
+        List<SpriteGridCell> cells;
+        List<SpriteGridTitle> titles;
+
+        float circle_radius;
+        float bg_rotation;
+        float bg_alpha = 0f;
+        Vector2 bg_origin;
+        static float bg_thickness = Celeste.TargetWidth;
+
+        public static float spacing = 70;
+        public float scroll_offset;
+        float left_start = Celeste.TargetWidth / 2 - 2.5f * SpriteGridCell.default_size * SpriteGridCell.upscale - spacing * 2;
+        int cursor_at;
+
         bool Focused;
 
-        // MTexture bg = Celeste.
+        float top_free_space = 90;
 
+        Vector2 new_cell_pos(float vert_offset = 0, int reset_at = 0)
+        {
+            float x = left_start + (cells.Count - reset_at) % 5 * (SpriteGridCell.upscale * SpriteGridCell.default_size + spacing);
+
+            // float y = vert_offset + (cells.Count - reset_at) / 5
+            //         * SpriteGridCell.upscale * SpriteGridCell.default_size
+            //         * 1.3f;
+            int ycc = reset_at % 5 == 0 ? cells.Count() : cells.Count - reset_at + 5 - reset_at % 5;
+            float y = SpriteGridCell.upscale * SpriteGridCell.default_size * 1.3f * (ycc / 5) + vert_offset;
+            // float y = SpriteGridCell.upscale * SpriteGridCell.default_size * 1.3f * (ycc / 5) + vert_offset;
+            // float y = top_free_space + SpriteGridCell.upscale * SpriteGridCell.default_size * 1.3f * (cells.Count() / 5);
+
+            return new(x, y);
+        }
 
         public override void Update()
         {
@@ -68,33 +66,171 @@ namespace Celeste.Mod.EmoteMod
 
             if (Focused)
             {
-                if (Input.MenuCancel.Pressed)
+                #region navigation
+                if (Input.MenuRight.Pressed)
+                {
+                    cells[cursor_at].Deselect();
+
+                    cursor_at++;
+                    if (cursor_at >= cells.Count)
+                        cursor_at = 0;
+
+                    cells[cursor_at].Select();
+                }
+                else if (Input.MenuLeft.Pressed)
+                {
+                    cells[cursor_at].Deselect();
+
+                    cursor_at--;
+                    if (cursor_at < 0)
+                        cursor_at = cells.Count - 1;
+
+                    cells[cursor_at].Select();
+                }
+                else if (Input.MenuUp.Pressed)
+                {
+                    cells[cursor_at].Deselect();
+
+                    if (cursor_at - 5 < 0)
+                    {
+                        int temp_pos = ((int)Math.Ceiling(cells.Count / 5f)) * 5 + cursor_at - 5;
+                        if (temp_pos >= cells.Count)
+                            cursor_at = temp_pos - 5;
+                        else
+                            cursor_at = temp_pos;
+                    }
+                    else
+                    {
+                        cursor_at -= 5;
+                    }
+
+                    cells[cursor_at].Select();
+                }
+                else if (Input.MenuDown.Pressed)
+                {
+                    cells[cursor_at].Deselect();
+
+                    if (cursor_at + 5 >= cells.Count)
+                        cursor_at %= 5;
+                    else
+                        cursor_at += 5;
+
+                    cells[cursor_at].Select();
+                }
+                #endregion
+
+                else if (Input.MenuCancel.Pressed)
                 {
                     coro = SpriteBankExit();
                 }
             }
         }
+
         public override void Render()
         {
             base.Render();
-            Draw.Rect(-10, -10, Celeste.TargetWidth + 20, Celeste.TargetHeight + 20, Color.Black * bg_alpha);
-
+            // Draw.Rect(-10, -10, Celeste.TargetWidth + 20, Celeste.TargetHeight + 20, Color.Black * bg_alpha);
+            Draw.LineAngle(bg_origin,
+                    bg_rotation, Celeste.TargetWidth * 2,
+                    Color.Black * bg_alpha, bg_thickness);
         }
 
         private IEnumerator SpriteBankEnter()
         {
             Visible = true;
             parent.stopVisibilityChecks = true;
-            // metallica - fade to black
-            for (float d = 0; d < 1; d += Engine.DeltaTime * 12)
-            {
-                bg_alpha = 0xff * d;
-                // EmoteModModule.echo($"alpha is {bg_alpha}");
-                yield return null;
 
+            // add not-so-customs
+            titles.Add(new("Visible to everyone", new(left_start, top_free_space), this, 0.125f + cells.Count * 0.02f));
+
+            float defaults_y_start = top_free_space + titles.Last().total_height;
+
+            foreach (string m in AnimationHelper.global_emotes.Keys.ToList())
+            {
+                cells.Add(new(AnimationHelper.GetInfo($"{m}:idle"), m, new_cell_pos(defaults_y_start), this, 0.125f + cells.Count * 0.02f)); // TODO: add fade in
             }
-            parent.Visible = false;
+
+            // collect customs
+            // why tf is it called modes here its not modes and its not even spritebanks
+            // ok
+            List<string> modes = GFX.SpriteBank.SpriteData.Keys
+                .Where(i => !AnimationHelper.global_emotes.ContainsKey(i))
+                .ToList();
+
+            // float side_size = SpriteGridCell.default_size * SpriteGridCell.upscale;
+
+            // add cells
+            int cells_reset = cells.Count;
+            Vector2 customs_start_pos = new_cell_pos(defaults_y_start, cells_reset);
+
+            titles.Add(new("Visible to EmoteMod users", new(left_start, customs_start_pos.Y), this, 0.125f + cells.Count * 0.02f)); // TODO: add fade in
+
+            float customs_y_start = defaults_y_start + titles.Last().total_height;
+            // float customs_y_start = customs_start_pos.Y + titles.Last().total_height;
+
+            EmoteModModule.echo($"start pos: {customs_start_pos.Y}, y start: {customs_y_start}");
+
+            foreach (string m in modes)
+            {
+                // skip the ones with no animations if those even exist
+                if (GFX.SpriteBank.SpriteData[m].Sprite.Animations.Count() == 0)
+                    continue;
+
+                List<string> anims = GFX.SpriteBank.SpriteData[m].Sprite.Animations.Keys.ToList();
+                string anim_name = anims.Contains("idle") ? "idle" : anims.First();
+                string full_name = $"{m}:{anim_name}";
+                EmoteInfo info = AnimationHelper.GetInfo(full_name);
+
+                if (info == null)
+                {
+                    Logger.Log(LogLevel.Info, "EmoteMod", $"could not get info for '{full_name}'");
+                    continue;
+                }
+                else if (info.isCustom)
+                {
+                    Emote.addCustomEmote(full_name);
+                    customsForRemoval.Add($"{full_name}");
+                }
+
+                cells.Add(new(info, m, new_cell_pos(customs_y_start, cells_reset), this, 0.125f + cells.Count * 0.02f));
+                // Scene.Add(cells.Last());
+            }
+
+
+            // lmaoo no rotating rectangles?
+            // too bad
+            // metallica - fade to black
+
+            // reset bg pos
+            bg_origin = new Vector2(-bg_thickness / 2, Celeste.TargetHeight);
             bg_alpha = 0xff;
+
+            // rotate the bg in
+            for (float d = 0; d < 1; d += Engine.DeltaTime * 4)
+            {
+                bg_rotation = ((float)Math.PI) / 2 * (d - 1);
+                bg_origin = new Vector2(
+                        ((float)Math.Sin(bg_rotation)) * bg_thickness / 2,
+                        Celeste.TargetHeight - bg_thickness / 2 * ((float)Math.Cos(bg_rotation))
+                        );
+
+                // EmoteModModule.echo($"bg:({bg_origin.X}, {bg_origin.Y}) at {bg_rotation}");
+                yield return null;
+            }
+
+            bg_origin = new Vector2(0, 5 + Celeste.TargetHeight - bg_thickness / 2);
+            bg_rotation = 0;
+
+            parent.Visible = false;
+
+
+
+            // obligatory wait
+            // for (float d = 0; d < 1; d += Engine.DeltaTime * 8)
+            //     yield return null;
+
+            cursor_at = 0;
+            cells[cursor_at].Select();
 
 
             Focused = true;
@@ -120,6 +256,7 @@ namespace Celeste.Mod.EmoteMod
             parent.atButton = Butt.Spritebank;
             parent.stopVisibilityChecks = false;
             parent.Visible = true;
+            Clear();
             RemoveSelf();
 
         }
@@ -127,21 +264,44 @@ namespace Celeste.Mod.EmoteMod
         void Init()
         {
             Tag = Tags.HUD;
+            customsForRemoval = new();
+            cells = new();
+            titles = new();
+        }
+
+        void Clear()
+        {
+            foreach (string e in customsForRemoval)
+            {
+                try
+                {
+                    Emote.madeline_bp.Remove(e);
+                }
+                catch { }
+            }
+            foreach (SpriteGridCell c in cells)
+            {
+                c.RemoveSelf();
+            }
+            foreach (SpriteGridTitle t in titles)
+            {
+                t.RemoveSelf();
+            }
         }
 
 
         /// <summary>
         /// <paramref name="spritebank"/> - the spritebank currently targeted
         /// <paramref name="emote"/> - the emote currently targeted
-		/// this is for emote only
-		/// </summary>
+        /// this is for emote only
+        /// </summary>
         public SpriteGrid(string spritebank, string emote, EmoteCard parent)
         {
         }
         /// <summary>
         /// <paramref name="spritebank"/> - the spritebank currently targeted
-		/// this is for spritebank + emote
-		/// </summary>
+        /// this is for spritebank + emote
+        /// </summary>
         public SpriteGrid(string spritebank, EmoteCard parent)
         {
             Init();
---
+++ UI/SpriteGridElements.cs
@@ -0,0 +1,202 @@
+using Monocle;
+using System.Collections.Generic;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Input;
+using System.Collections;
+using System;
+using System.Reflection;
+using System.Linq;
+
+namespace Celeste.Mod.EmoteMod
+{
+    public abstract class SpriteGridElement : Entity
+    {
+        internal IEnumerator coro;
+        internal float fade_in_delay;
+        internal float alpha;
+        internal Vector2 Pos;
+        internal SpriteGrid parent;
+        internal float scroll_shift => parent.scroll_offset;
+
+        private float visible_margin = 500;
+
+        public IEnumerator FadeIn()
+        {
+            while (fade_in_delay > 0)
+            {
+                fade_in_delay -= Engine.DeltaTime;
+                yield return null;
+            }
+            for (float d = 0; d < 1; d += Engine.DeltaTime * 8)
+            {
+                alpha = d;
+                yield return null;
+            }
+            alpha = 1;
+
+            yield return null;
+        }
+
+        public override void Update()
+        {
+            base.Update();
+
+            if (coro != null)
+                coro.MoveNext();
+
+            if (Pos.X < -visible_margin
+                    || Pos.X > Celeste.TargetWidth
+                    || Pos.Y < -visible_margin
+                    || Pos.Y > Celeste.TargetHeight)
+            {
+                Visible = false;
+            }
+            else
+            {
+                Visible = true;
+            }
+        }
+
+        private IEnumerator fadeOut(string direction)
+        {
+            yield return null;
+        }
+
+        public void FadeOut(string direction = "none")
+        {
+            coro = fadeOut(direction);
+        }
+    }
+
+    public class SpriteGridTitle : SpriteGridElement
+    {
+        string text;
+        public static Vector2 text_scale = Vector2.One;
+        public float text_height;
+        public static float line_offset = 15;
+        public float total_height => text_height + line_offset + 5 + line_offset;
+        // string bottom_text = "";
+
+        public override void Render()
+        {
+            base.Render();
+
+            ActiveFont.DrawOutline(text, Pos, Vector2.Zero, text_scale, Color.Snow * alpha, 3, Color.Black * alpha);
+            Draw.Line(new Vector2(Pos.X, Pos.Y + text_height + line_offset)
+                    , new Vector2(Celeste.TargetWidth - Pos.X, Pos.Y + text_height + line_offset)
+                    , Color.Snow * alpha, 5);
+            // Draw.Line(SpriteGridCell.)
+        }
+
+        void Init(Vector2 pos, string text, SpriteGrid parent, float fadeInDelay)
+        {
+            this.Pos = pos;
+            this.text = text;
+            this.parent = parent;
+            this.fade_in_delay = fadeInDelay;
+            parent.Scene.Add(this);
+            this.text_height = (ActiveFont.Measure(text) * text_scale).Y;
+            Tag = Tags.HUD;
+            coro = FadeIn();
+        }
+
+        public SpriteGridTitle(string text, Vector2 pos, SpriteGrid parent, float fadeInDelay = 0)
+        {
+            Init(pos, text, parent, fadeInDelay);
+        }
+        // public SpriteGridTitle(Vector2 pos, string text, string subtext, SpriteGrid parent)
+        // {
+        //     Init(pos, text, parent);
+        //
+        // }
+    }
+
+    public class SpriteGridCell : SpriteGridElement
+    {
+        PlayerSprite sprite;
+        string text;
+        Wiggler wiggler;
+        bool isSelected;
+        float downscale;
+        Color flashing_color => !Settings.Instance.DisableFlashes && !this.Scene.BetweenInterval(0.1f) ? TextMenu.HighlightColorB : TextMenu.HighlightColorA;
+
+
+        public static float default_size = 32;
+        public static float upscale = 6;
+
+        public void Select()
+        {
+            isSelected = true;
+        }
+
+        public void Deselect()
+        {
+            isSelected = false;
+        }
+
+        public override void Update()
+        {
+            base.Update();
+
+            // if (coro != null)
+            //     coro.MoveNext();
+            //
+            // sprite.Color = Color.White * alpha;
+            //
+            // sprite.Position = Pos;
+            sprite.Update();
+        }
+
+        public override void Render()
+        {
+            base.Render();
+            HudRenderer.EndRender();
+            HudRenderer.BeginRender(null, Microsoft.Xna.Framework.Graphics.SamplerState.PointClamp);
+
+            sprite.Position = Pos;
+            sprite.Color = Color.White * alpha;
+
+            sprite.Render();
+            float rect_side = default_size * upscale;
+            Draw.HollowRect(Pos, rect_side, rect_side, Color.Snow * alpha);
+
+            ActiveFont.DrawOutline(text, Pos + new Vector2(rect_side / 2, rect_side),
+                    new Vector2(0.5f, 0), Vector2.One * 0.5f,
+                    isSelected ? flashing_color : Color.Snow * alpha, 3, Color.Black * alpha);
+
+            HudRenderer.EndRender();
+            HudRenderer.BeginRender();
+
+
+        }
+
+        public SpriteGridCell(EmoteInfo emote, string text, Vector2 pos, SpriteGrid parent, float fadeInDelay = 0f)
+        {
+            Tag = Tags.HUD;
+            this.Pos = pos;
+            this.text = text;
+            fade_in_delay = fadeInDelay;
+            alpha = 0;
+            coro = FadeIn();
+
+            this.sprite = new(emote.spritemode);
+
+            string anim_name = AnimationHelper.global_emotes.ContainsKey(emote.spritebank)
+                ? emote.animation
+                : emote.isCustom ? $"{emote.spritebank}:{emote.animation}" : emote.animation;
+            MTexture first_frame = sprite.Animations[anim_name].Frames[0];
+
+            float max_side = Math.Max(first_frame.Width, first_frame.Height);
+            downscale = default_size / max_side;
+            sprite.Scale = Vector2.One * downscale * upscale;
+            sprite.JustifyOrigin(0, 0);
+
+            sprite.Play(anim_name);
+            parent.Scene.Add(this);
+        }
+    }
+
+
+
+}
+
