2aa6b3282d3e86cb5a9e1ebde65a2d9e7943e104
Desant <anturated@gmail.com>
2025-08-03 23:27:56 +0200

    format

--- EmoteWheel.cs
+++ EmoteWheel.cs
@@ -10,21 +10,21 @@ using Microsoft.Xna.Framework;
 namespace Celeste.Mod.EmoteMod
 {
     public static class EmoteWheel
-	{
+    {
 
-		public static VirtualJoystick JoystickEmoteWheel;
-		public static VirtualButton ButtonEmoteSend;
+        public static VirtualJoystick JoystickEmoteWheel;
+        public static VirtualButton ButtonEmoteSend;
 
         public static EmoteWheelObject Wheel;
 
         public static bool activatedWithButton;
         public static bool joystickMoved;
 
-		private static ILHook celestenetUpdateEmoteWheelHook;
-		internal static int onCNetWheelUpdate;
+        private static ILHook celestenetUpdateEmoteWheelHook;
+        internal static int onCNetWheelUpdate;
 
-		private static void Player_Update(On.Celeste.Player.orig_Update orig, Player self)
-		{
+        private static void Player_Update(On.Celeste.Player.orig_Update orig, Player self)
+        {
             orig(self);
 
             Player Player = self;
@@ -50,14 +50,14 @@ namespace Celeste.Mod.EmoteMod
             if (JoystickEmoteWheel == null)
                 goto End;
 
-            
+
 
 
             // TimeRate check is for Prologue Dash prompt freeze
             if (!level.Paused && !Player.Dead && Engine.TimeRate > 0.05f)
             {
                 // cool key feture
-                if (EmoteModModule.Settings.EmoteWheelBinding.Buttons.Count != 0 
+                if (EmoteModModule.Settings.EmoteWheelBinding.Buttons.Count != 0
                 && MInput.GamePads[0].Pressed(EmoteModModule.Settings.EmoteWheelBinding.Buttons[0]))
                 {
                     activatedWithButton = !activatedWithButton;
@@ -78,7 +78,7 @@ namespace Celeste.Mod.EmoteMod
                     joystickMoved = false;
                 }
 
-				int selected = Wheel.Selected;
+                int selected = Wheel.Selected;
                 if (Wheel.Shown && selected != -1 && ButtonEmoteSend.Pressed)
                 {
                     Send(selected, Player);
@@ -95,7 +95,7 @@ namespace Celeste.Mod.EmoteMod
                 joystickMoved = false;
             }
 
-            End:
+        End:
             return;
             //if (Wheel?.Shown ?? false)
             //    Context.Main.StateUpdated |= Context.Main.ForceIdle.Add("EmoteWheel");
@@ -106,38 +106,38 @@ namespace Celeste.Mod.EmoteMod
 
 
         public static void Send(int index, Player player)
-		{
-			string[] emotes = 
+        {
+            string[] emotes =
             {
-				EmoteModModule.Settings.emote0,
-				EmoteModModule.Settings.emote1,
-				EmoteModModule.Settings.emote2,
-				EmoteModModule.Settings.emote3,
-				EmoteModModule.Settings.emote4,
-				EmoteModModule.Settings.emote5,
-				EmoteModModule.Settings.emote6,
-				EmoteModModule.Settings.emote7,
-				EmoteModModule.Settings.emote8,
-				EmoteModModule.Settings.emote9
-			};
+                EmoteModModule.Settings.emote0,
+                EmoteModModule.Settings.emote1,
+                EmoteModModule.Settings.emote2,
+                EmoteModModule.Settings.emote3,
+                EmoteModModule.Settings.emote4,
+                EmoteModModule.Settings.emote5,
+                EmoteModModule.Settings.emote6,
+                EmoteModModule.Settings.emote7,
+                EmoteModModule.Settings.emote8,
+                EmoteModModule.Settings.emote9
+            };
             if (0 <= index && index < emotes.Length)
-                Emote.DoEmote(emotes[index], false, player);
-		}
+                Emote.DoEmote(EmoteModModule.Settings.Emotes[index].GetInfo(), false, player); // TODO
+        }
 
-		private static void Input_Initialize(On.Celeste.Input.orig_Initialize orig)
-		{
+        private static void Input_Initialize(On.Celeste.Input.orig_Initialize orig)
+        {
             orig();
 
-			JoystickEmoteWheel = new VirtualJoystick(true,
-				new VirtualJoystick.PadRightStick(Input.Gamepad, 0.2f)
-			);
-			ButtonEmoteSend = new VirtualButton(
-				new VirtualButton.KeyboardKey(Keys.Q),
-				new VirtualButton.PadButton(Input.Gamepad, Buttons.RightStick)
-			);
+            JoystickEmoteWheel = new VirtualJoystick(true,
+                new VirtualJoystick.PadRightStick(Input.Gamepad, 0.2f)
+            );
+            ButtonEmoteSend = new VirtualButton(
+                new VirtualButton.KeyboardKey(Keys.Q),
+                new VirtualButton.PadButton(Input.Gamepad, Buttons.RightStick)
+            );
 
             activatedWithButton = false;
-		}
+        }
 
         private static void celestenetUpdateEmoteWheel(ILContext il)
         {
@@ -151,58 +151,58 @@ namespace Celeste.Mod.EmoteMod
             }
         }
 
-		private static void OnHeartGemCollect(On.Celeste.HeartGem.orig_Collect orig, HeartGem self, Player player)
-		{
-			orig(self, player);
-			Wheel?.TimeRateSkip.Add(self.IsFake ? "EmptySpaceHeart" : "HeartGem");
-			if (self.IsFake && Wheel != null)
-				Wheel.timeSkipForcedDelay = 10f;
-		}
+        private static void OnHeartGemCollect(On.Celeste.HeartGem.orig_Collect orig, HeartGem self, Player player)
+        {
+            orig(self, player);
+            Wheel?.TimeRateSkip.Add(self.IsFake ? "EmptySpaceHeart" : "HeartGem");
+            if (self.IsFake && Wheel != null)
+                Wheel.timeSkipForcedDelay = 10f;
+        }
 
-		private static void OnHeartGemEndCutscene(On.Celeste.HeartGem.orig_EndCutscene orig, HeartGem self)
-		{
-			orig(self);
-			Wheel?.TimeRateSkip.Remove("HeartGem");
-		}
+        private static void OnHeartGemEndCutscene(On.Celeste.HeartGem.orig_EndCutscene orig, HeartGem self)
+        {
+            orig(self);
+            Wheel?.TimeRateSkip.Remove("HeartGem");
+        }
 
-		private static PlayerDeadBody OnPlayerDie(On.Celeste.Player.orig_Die orig, Player self, Vector2 direction, bool evenIfInvincible, bool registerDeathInStats)
-		{
-			PlayerDeadBody pdb = orig(self, direction, evenIfInvincible, registerDeathInStats);
-			if (pdb != null && Wheel != null)
-			{
-				Wheel.TimeRateSkip.Add("PlayerDead");
-				Wheel.Shown = false;
-				Wheel.ForceSetTimeRate = true;
+        private static PlayerDeadBody OnPlayerDie(On.Celeste.Player.orig_Die orig, Player self, Vector2 direction, bool evenIfInvincible, bool registerDeathInStats)
+        {
+            PlayerDeadBody pdb = orig(self, direction, evenIfInvincible, registerDeathInStats);
+            if (pdb != null && Wheel != null)
+            {
+                Wheel.TimeRateSkip.Add("PlayerDead");
+                Wheel.Shown = false;
+                Wheel.ForceSetTimeRate = true;
                 activatedWithButton = false;
-			}
-			return pdb;
-		}
+            }
+            return pdb;
+        }
 
-		internal static void Load()
-		{
+        internal static void Load()
+        {
             On.Celeste.Input.Initialize += Input_Initialize;
             // CHANGE!!!!!!!!
             On.Celeste.Player.Update += Player_Update;
 
-			On.Celeste.HeartGem.Collect += OnHeartGemCollect;
-			On.Celeste.HeartGem.EndCutscene += OnHeartGemEndCutscene;
-			On.Celeste.Player.Die += OnPlayerDie;
+            On.Celeste.HeartGem.Collect += OnHeartGemCollect;
+            On.Celeste.HeartGem.EndCutscene += OnHeartGemEndCutscene;
+            On.Celeste.Player.Die += OnPlayerDie;
 
-			celestenetUpdateEmoteWheelHook = new ILHook(typeof(CelesteNetEmoteComponent).GetMethod("Update"), celestenetUpdateEmoteWheel);
+            celestenetUpdateEmoteWheelHook = new ILHook(typeof(CelesteNetEmoteComponent).GetMethod("Update"), celestenetUpdateEmoteWheel);
 
         }
 
 
-		internal static void Unload()
-		{
-			On.Celeste.Input.Initialize -= Input_Initialize;
-			On.Celeste.Player.Update -= Player_Update;
+        internal static void Unload()
+        {
+            On.Celeste.Input.Initialize -= Input_Initialize;
+            On.Celeste.Player.Update -= Player_Update;
 
-			On.Celeste.HeartGem.Collect -= OnHeartGemCollect;
-			On.Celeste.HeartGem.EndCutscene -= OnHeartGemEndCutscene;
-			On.Celeste.Player.Die -= OnPlayerDie;
+            On.Celeste.HeartGem.Collect -= OnHeartGemCollect;
+            On.Celeste.HeartGem.EndCutscene -= OnHeartGemEndCutscene;
+            On.Celeste.Player.Die -= OnPlayerDie;
 
-			celestenetUpdateEmoteWheelHook.Dispose();
+            celestenetUpdateEmoteWheelHook.Dispose();
         }
-	}
+    }
 }
